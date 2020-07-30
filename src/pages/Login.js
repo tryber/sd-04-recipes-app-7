d25004674b58d9ac02f57937b1d0cefe9af7b556
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-30 20:02:29 -0300

    Criado componente de Login e implementado.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+
+const validEmail = '^([a-zd.-]+)@([a-zd-]+).([a-z]{2,8})(.[a-z]{2,8})?$';
 
 const Login = () => {
-  return <div></div>;
+  const defaultData = { email: '', pass: '' };
+  const [{ email, pass }, setUserData] = useState(defaultData); // userData
+  const handleInputs = ({ target: { value, name } }) => {
+    setUserData({ ...defaultData, [name]: value });
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        name="email"
+        value={email}
+        onChange={handleInputs}
+        pattern={validEmail}
+      />
+      <input
+        type="password"
+        name="pass"
+        value={pass}
+        onChange={handleInputs}
+        data-testid="password-input"
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={email && pass}
+      >
+        Entrar
+      </button>
+    </div>
+  );
 };
 
 export default Login;
