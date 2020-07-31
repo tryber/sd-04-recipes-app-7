b808930844e451d2eb238bbba0f1a66dba594edd
fogo5000 <lucaslucas2008@gmail.com>
2020-07-31 16:01:26 -0300

    Fixx 'Router' to 'Route'

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter, Router } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from './context';
 import * as pages from './pages';
 import './App.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Provider>
         <Switch>
-          <Router exact path='/' component={pages.Login} />
-          <Router exact path='/comidas' />
-          <Router exact path='/bebidas' />
-          <Router exact path='/comidas/:id' />
-          <Router exact path='/bebidas/:id' />
-          <Router path='/comidas/:id/in-progress' />
-          <Router path='/bebidas/:id/in-progress' />
-          <Router exact path='/explorar' />
-          <Router exact path='/explorar/comidas' />
-          <Router exact path='/explorar/bebidas' />
-          <Router path='/explorar/comidas/ingredientes' />
-          <Router path='/explorar/bebidas/ingredientes' />
-          <Router path='/explorar/comidas/area' />
-          <Router path='/perfil' />
-          <Router path='/receitas-feitas' />
-          <Router path='/receitas-favoritas' />
+          <Route exact path='/' component={pages.Login} />
+          <Route exact path='/comidas' />
+          <Route exact path='/bebidas' />
+          <Route exact path='/comidas/:id' />
+          <Route exact path='/bebidas/:id' />
+          <Route path='/comidas/:id/in-progress' />
+          <Route path='/bebidas/:id/in-progress' />
+          <Route exact path='/explorar' />
+          <Route exact path='/explorar/comidas' />
+          <Route exact path='/explorar/bebidas' />
+          <Route path='/explorar/comidas/ingredientes' />
+          <Route path='/explorar/bebidas/ingredientes' />
+          <Route path='/explorar/comidas/area' />
+          <Route path='/perfil' />
+          <Route path='/receitas-feitas' />
+          <Route path='/receitas-favoritas' />
         </Switch>
       </Provider>
-    </BrowserRouter>
+    </Router>
   );
 };
 
