b34e5c2f7d6373c1c26274cc6ec257c81ff53657
wistyk <leonardoxw@gmail.com>
2020-08-04 20:53:28 -0300

    pendente validaçao de bebidas para o redirect

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Redirect } from 'react-router';
 import {
   getFilterByIngredient,
   getMealByNameAPI,
@@ -8,37 +9,24 @@ import {
 const radioOption = (value, selection, setSelection) => {
   if (selection.length >= 1) {
     switch (value) {
+      case !('ingredient' || 'name' || 'firstLetter'):
+        selection = null; break;
       case 'ingredient':
         getFilterByIngredient(selection).then((data) => setSelection(data.meals),
         ); break;
       case 'name':
         getMealByNameAPI(selection).then((data) => setSelection(data.meals)); break;
-      case value:
+      case 'firstLetter':
+        if (selection.length > 1) alert('Digite apenas uma letra')
         getFilterByFirstLetter(selection).then((data) => setSelection(data.meals),
         ); break;
       default:
-        console.log('');
+        selection = null;
     }
   } else {
-    console.log('Escolha um ingrediente');
+    alert('Escolha um ingrediente');
   }
-};
-
-const onClick = (selection) => {
-  if (selection === null) alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
-  if (selection.length === 1) {
-    const mainPage = document.querySelector('#root');
-    const list = document.createElement('UL');
-    selection.map((item) => {
-      list.setAttribute('key', `${item.strMeal}`);
-      mainPage.appendChild(list);
-      const mealList = document.createElement('LI');
-      const meal = document.createTextNode(`${item.strMeal}`);
-      mealList.appendChild(meal);
-      return list.appendChild(mealList);
-    });
-  }
-  console.log('a lot');
+  return value = '';
 };
 
 const SearchBar = () => {
@@ -48,34 +36,40 @@ const SearchBar = () => {
     setSelection(event);
   };
 
+  const onClick = () => {
+    const mainPage = document.querySelector('#root');
+    const list = document.createElement('UL');
+    if (selection === null || selection.length === 0)
+    alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+    if (selection.length === 1 && <Redirect to="/" />);
+    if (selection.length > 1) {
+      selection.map((item) => {
+        list.setAttribute('key', `${item.strMeal}`);
+        mainPage.appendChild(list)
+        const mealList = document.createElement('LI');
+        const meal = document.createTextNode(`${item.strMeal}`);
+        mealList.appendChild(meal);
+        return list.appendChild(mealList);
+      });
+    };
+  };
   return (
     <div>
       <input
-        data-testid="search-input"
-        placeholder="Buscar Receita"
-        id="searchBar"
-        type="text"
+        data-testid="search-input" placeholder="Buscar Receita" id="searchBar" type="text"
         onChange={(event) => setText(event.target.value)}
       />
       <input
-        data-testid="ingredient-search-radio"
-        id="ingredient"
-        type="radio"
-        name="filter"
+        data-testid="ingredient-search-radio" id="ingredient" type="radio" name="filter"
         onChange={() => radioOption('ingredient', selection, setSelection)}
       />
       <label htmlFor="ingredient">Ingrediente</label>
       <input
-        data-testid="name-search-radio"
-        id="name"
-        type="radio"
-        name="filter"
+        data-testid="name-search-radio" id="name" type="radio" name="filter"
         onChange={() => radioOption('name', selection, setSelection)}
       />
       <label htmlFor="name">Nome</label>
-      <input
-        data-testid="first-letter-search-radio" id="firstLetter"
-        type="radio" name="filter"
+      <input data-testid="first-letter-search-radio" id="firstLetter" type="radio" name="filter"
         onChange={() => radioOption('firstLetter', selection, setSelection)}
       />
       <label htmlFor="firstLetter">Primeira letra</label>
