bda8f21f224aea0bdf4f124464c48af59a08f156
Thiago Tavares <thiagooo15@gmail.com>
2020-08-04 20:44:25 -0300

    cc

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -31,45 +31,20 @@ const SearchBar = () => {
     setSelection(event);
   };
 
-  const onClick = () => {
-    if (selection === null) alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
-    if (selection.length === 1) {
-      let mainPage = document.querySelector('#root');
-      selection.map((item) => {
-      let list = document.createElement('UL');
-      list.setAttribute('key', `${item.strMeal}`);
-      mainPage.appendChild(list)
-      let mealList = document.createElement('LI');
-      let meal = document.createTextNode(`${item.strMeal}`);
-      mealList.appendChild(meal);
-      list.appendChild(mealList);
-    });
-    };
-    console.log('a lot');
-  };
   return (
     <div>
       <input
-        data-testid="search-input"
-        placeholder="Buscar Receita"
-        id="searchBar"
-        type="text"
-        onChange={(event) => setText(event.target.value)}
+        data-testid="search-input" placeholder="Buscar Receita" id="searchBar"
+        type="text" onChange={(event) => setText(event.target.value)}
       />
       <input
-        data-testid="ingredient-search-radio"
-        id="ingredient"
-        type="radio"
-        name="filter"
-        onChange={() => radioOption('ingredient', selection, setSelection)}
+        data-testid="ingredient-search-radio" id="ingredient" type="radio"
+        name="filter" onChange={() => radioOption('ingredient', selection, setSelection)}
       />
       <label htmlFor="ingredient">Ingrediente</label>
       <input
-        data-testid="name-search-radio"
-        id="name"
-        type="radio"
-        name="filter"
-        onChange={() => radioOption('name', selection, setSelection)}
+        data-testid="name-search-radio" id="name" type="radio"
+        name="filter" onChange={() => radioOption('name', selection, setSelection)}
       />
       <label htmlFor="name">Nome</label>
       <input
@@ -78,7 +53,7 @@ const SearchBar = () => {
         onChange={() => radioOption('firstLetter', selection, setSelection)}
       />
       <label htmlFor="firstLetter">Primeira letra</label>
-      <button data-testid="exec-search-btn" type="button" onClick={onClick}>
+      <button data-testid="exec-search-btn" type="button">
         Buscar
       </button>
     </div>
