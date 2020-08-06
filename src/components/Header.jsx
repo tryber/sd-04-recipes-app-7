63beda0140b5f4f67f833aa5fecc12d4c28c4e10
Thiago Tavares <thiagooo15@gmail.com>
2020-08-06 17:08:44 -0300

    last push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import './header.css';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import SearchBar from './SearchBar';
