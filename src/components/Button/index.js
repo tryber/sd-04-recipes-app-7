650346f931286f3914999c9dfaf235780a4872fe
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-08 17:17:53 -0300

    CCzão.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,21 +4,34 @@ import { getMealsByCategoryAPI, getCocktailByCategoryAPI } from '../../services'
 
 import './styles.css';
 
+const auxMeals = (strCategory, idKey, setIdKey, category, setCategory) => {
+  if (idKey !== strCategory) {
+    setIdKey(strCategory);
+    getMealsByCategoryAPI(strCategory).then((resp) => setCategory([...resp.meals]));
+  } else {
+    getMealsByCategoryAPI(strCategory).then((resp) =>
+      (category.length ? setCategory([]) : setCategory([...resp.meals])),
+    );
+  }
+};
+
+const auxCockTails = (strCategory, idKey, setIdKey, category, setCategory) => {
+  if (idKey !== strCategory) {
+    setIdKey(strCategory);
+    getCocktailByCategoryAPI(strCategory).then((resp) => setCategory([...resp.drinks]));
+  } else {
+    getCocktailByCategoryAPI(strCategory).then((resp) =>
+      (category.length ? setCategory([]) : setCategory([...resp.drinks])),
+    );
+  }
+};
+
 const meals = (strCategory, idKey, setIdKey, category, setCategory) => (
   <button
     type="button"
     data-testid={`${strCategory}-category-filter`}
     className="category-filter"
-    onClick={() => {
-      if (idKey !== strCategory) {
-        setIdKey(strCategory);
-        getMealsByCategoryAPI(strCategory).then((resp) => setCategory([...resp.meals]));
-      } else {
-        getMealsByCategoryAPI(strCategory).then((resp) =>
-          category.length ? setCategory([]) : setCategory([...resp.meals]),
-        );
-      }
-    }}
+    onClick={() => auxMeals(strCategory, idKey, setIdKey, category, setCategory)}
   >
     {strCategory}
   </button>
@@ -29,16 +42,16 @@ const cockTails = (strCategory, idKey, setIdKey, category, setCategory) => (
     type="button"
     data-testid={`${strCategory}-category-filter`}
     className="category-filter"
-    onClick={() => {
-      if (idKey !== strCategory) {
-        setIdKey(strCategory);
-        getCocktailByCategoryAPI(strCategory).then((resp) => setCategory([...resp.drinks]));
-      } else {
-        getCocktailByCategoryAPI(strCategory).then((resp) =>
-          category.length ? setCategory([]) : setCategory([...resp.drinks]),
-        );
-      }
-    }}
+    onClick={() => auxCockTails(strCategory, idKey, setIdKey, category, setCategory)
+      // if (idKey !== strCategory) {
+      //   setIdKey(strCategory);
+      //   getCocktailByCategoryAPI(strCategory).then((resp) => setCategory([...resp.drinks]));
+      // } else {
+      //   getCocktailByCategoryAPI(strCategory).then((resp) =>
+      //     (category.length ? setCategory([]) : setCategory([...resp.drinks])),
+      //   );
+      // }
+    }
   >
     {strCategory}
   </button>
