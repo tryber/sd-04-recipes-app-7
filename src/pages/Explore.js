fdf7ae127273bedcb882ebb9c3f47f4bd21e256b
wistyk <leonardoxw@gmail.com>
2020-08-11 17:47:28 -0300

    botoes funcionando

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import BottomBar from '../components/BottomBar/BottomBar';
 import Header from '../components/Header';
+import { useHistory } from 'react-router-dom';
 
-const Explore = () => (
-  <div>
-    <Header title="Explorar" />
-    <BottomBar />
-  </div>
-);
+const Explore = () => {
+  const history = useHistory();
+  return (
+    <div>
+      <Header title="Explorar" />
+      <button
+        data-testid="explore-food"
+        onClick={() => history.push('/explorar/comidas')}
+      >
+        Explorar Comidas
+      </button>
+      <button
+        data-testid="explore-drinks"
+        onClick={() => history.push('/explorar/bebidas')}
+      >
+        Explorar Bebidas
+      </button>
+      <BottomBar />
+    </div>
+  );
+};
 
 export default Explore;
