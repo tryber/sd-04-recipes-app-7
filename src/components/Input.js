a245661acb3b228788fe15ccc38d78ab4d3adb1d
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-10 22:32:23 -0300

    A maioria dos issues resolvida.

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,51 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({ searchTerm, setSearchTerm, typeSearch, setTypeSearch, verify }) => {
-  return (
-    <div>
-      <input
-        data-testid="search-input"
-        placeholder="Buscar Receita"
-        id="searchBar"
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <input
-        data-testid="ingredient-search-radio"
-        id="ingredient"
-        type="radio"
-        name="filter"
-        onChange={(e) => setTypeSearch(e.target.id)}
-      />
-      <label htmlFor="ingredient">Ingrediente</label>
-      <input
-        data-testid="name-search-radio"
-        id="name"
-        type="radio"
-        name="filter"
-        onChange={(e) => setTypeSearch(e.target.id)}
-      />
-      <label htmlFor="name">Nome</label>
-      <input
-        data-testid="first-letter-search-radio"
-        id="firstLetter"
-        type="radio"
-        name="filter"
-        onChange={(e) => setTypeSearch(e.target.id)}
-      />
-      <label htmlFor="firstLetter">Primeira letra</label>
-      <button
-        data-testid="exec-search-btn"
-        type="button"
-        onClick={() => verify(searchTerm, typeSearch)}
-      >
-        Buscar
-      </button>
-    </div>
-  );
-};
+const Input = ({ searchTerm, setSearchTerm, typeSearch, setTypeSearch, verify }) => (
+  <div>
+    <input
+      data-testid="search-input"
+      placeholder="Buscar Receita"
+      id="searchBar"
+      type="text"
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+    />
+    <input
+      data-testid="ingredient-search-radio"
+      id="ingredient"
+      type="radio"
+      name="filter"
+      onChange={(e) => setTypeSearch(e.target.id)}
+    />
+    <label htmlFor="ingredient">Ingrediente</label>
+    <input
+      data-testid="name-search-radio"
+      id="name"
+      type="radio"
+      name="filter"
+      onChange={(e) => setTypeSearch(e.target.id)}
+    />
+    <label htmlFor="name">Nome</label>
+    <input
+      data-testid="first-letter-search-radio"
+      id="firstLetter"
+      type="radio"
+      name="filter"
+      onChange={(e) => setTypeSearch(e.target.id)}
+    />
+    <label htmlFor="firstLetter">Primeira letra</label>
+    <button
+      data-testid="exec-search-btn"
+      type="button"
+      onClick={() => verify(searchTerm, typeSearch)}
+    >
+      Buscar
+    </button>
+  </div>
+);
 
 Input.propTypes = {
   searchTerm: PropTypes.string.isRequired,
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -22,9 +22,9 @@ const SearchBar = () => {
     switch (flag) {
       case 'meals': {
         if (resultArray && resultArray.length === 1) return history.push(`/comidas/${resultArray[0].idMeal}`);
-        else if (!resultArray || resultArray.length === 0)
+        else if (!resultArray || resultArray.length === 0) {
           alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
-        else setFoodsCategory([...resultArray]);
+        } else setFoodsCategory([...resultArray]);
         break;
       }
       default:
@@ -34,32 +34,32 @@ const SearchBar = () => {
           alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
         } else setDrinksCategory([...resultArray]);
     }
-    return;
+    return null;
   };
 
-  const verifyTextMeals = (text, typeSearch) => {
-    if (typeSearch === 'ingredient') {
+  const verifyTextMeals = (text, radioValue) => {
+    if (radioValue === 'ingredient') {
       getMealByIngredient(text).then((resp) => verifyLength(resp.meals, 'meals'));
-    } else if (typeSearch === 'name') {
+    } else if (radioValue === 'name') {
       getMealByNameAPI(text).then((resp) => verifyLength(resp.meals, 'meals'));
     } else getMealByFirstLetter(text).then((resp) => verifyLength(resp.meals, 'meals'));
   };
 
-  const verifyTextCocktails = (text, typeSearch) => {
-    if (typeSearch === 'ingredient') {
+  const verifyTextCocktails = (text, radioValue) => {
+    if (radioValue === 'ingredient') {
       getCocktailByIngredient(text).then((resp) => verifyLength(resp.drinks, 'drinks'));
-    } else if (typeSearch === 'name') {
+    } else if (radioValue === 'name') {
       getCocktailByNameAPI(text).then((resp) => verifyLength(resp.drinks, 'drinks'));
     } else getCocktailByFirstLetter(text).then((resp) => verifyLength(resp.drinks, 'drinks'));
   };
 
-  const allowSearch = (text, typeSearch) => !(typeSearch === 'firstLetter' && text.length > 1);
+  const allowSearch = (text, radioValue) => !(radioValue === 'firstLetter' && text.length > 1);
 
-  const verifyPath = (text, typeSearch) => {
-    if (allowSearch(text, typeSearch)) {
-      if (window.location.pathname === '/comidas' && allowSearch(text, typeSearch)) {
-        verifyTextMeals(text, typeSearch);
-      } else verifyTextCocktails(text, typeSearch);
+  const verifyPath = (text, radioValue) => {
+    if (allowSearch(text, radioValue)) {
+      if (window.location.pathname === '/comidas' && allowSearch(text, radioValue)) {
+        verifyTextMeals(text, radioValue);
+      } else verifyTextCocktails(text, radioValue);
     } else alert('Sua busca deve conter somente 1 (um) caracter');
   };
 
