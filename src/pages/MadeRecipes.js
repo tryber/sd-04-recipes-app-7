f78e5933bd876ffc97ffa8301bc2dfd2dad8565d
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-11 15:29:23 -0300

    Criados botões da tela de receitas feitas.

--- src/pages/MadeRecipes.js
+++ src/pages/MadeRecipes.js
@@ -4,6 +4,9 @@ import Header from '../components/Header';
 const MadeRecipes = () => (
   <div>
     <Header title="Receitas Feitas" />
+    <button data-testid="filter-by-all-btn">All</button>
+    <button data-testid="filter-by-food-btn">Food</button>
+    <button data-testid="filter-by-drink-btn">Drinks</button>
   </div>
 );
 
