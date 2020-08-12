10662a931627c3fce36b4d2222856410ef0dffe6
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-11 21:12:15 -0300

    Sheipando o Props com shape.

--- src/components/DoneRecipesCards/index.js
+++ src/components/DoneRecipesCards/index.js
@@ -48,7 +48,17 @@ const DoneRecipesCard = ({
 
 DoneRecipesCard.propTypes = {
   index: PropTypes.number.isRequired,
-  recipe: PropTypes.object.isRequired,
+  recipe: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
+    area: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    alcoholicOrNot: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    doneDate: PropTypes.string.isRequired,
+    tags: PropTypes.array.isRequired,
+  }),
 };
 
 export default DoneRecipesCard;
