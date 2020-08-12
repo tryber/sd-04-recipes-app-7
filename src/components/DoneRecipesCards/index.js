0c2ab5b2425373e0dff486838bbfad3f83faddff
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-11 21:08:03 -0300

    Arrumando CC.

--- src/components/DoneRecipesCards/index.js
+++ src/components/DoneRecipesCards/index.js
@@ -1,57 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import shareIcon from '../../images/shareIcon.svg';
 import './styles.css';
 
-// const decideToShow = (type, area, category, alcoholicOrNot) => {
-//   if (type === 'comida') {
-//     return `${area} - ${category}`;
-//   }
-//   return `${alcoholicOrNot}`;
-// };
-
 const DoneRecipesCard = ({
   recipe: { id, type, area, category, alcoholicOrNot, name, image, doneDate, tags },
   index,
-}) => {
-  return (
-    <div className="done-recipes-container">
-      <Link to={`/${type}s/${id}`} className="link-img-recipe">
-        <img data-testid={`${index}-horizontal-image`} src={image} alt={name} />
-      </Link>
-      <div className="recipes-info">
-        <div className="share-container">
-          <span data-testid={`${index}-horizontal-top-text`}>
-            {type === 'comida' ? `${area} - ${category}` : `${alcoholicOrNot}`}
-          </span>
-          <img data-testid={`${index}-horizontal-share-btn`} src={shareIcon} alt="Share Recipe" />
-        </div>
-        <Link
-          to={`/${type}s/${id}`}
-          data-testid={`${index}-horizontal-name`}
-          className="recipe-title"
-        >
-          {name}
-        </Link>
-        <span data-testid={`${index}-horizontal-done-date`} className="date-made">
-          Feita em: {doneDate}
+}) => (
+  <div className="done-recipes-container">
+    <Link to={`/${type}s/${id}`} className="link-img-recipe">
+      <img data-testid={`${index}-horizontal-image`} src={image} alt={name} />
+    </Link>
+    <div className="recipes-info">
+      <div className="share-container">
+        <span data-testid={`${index}-horizontal-top-text`}>
+          {type === 'comida' ? `${area} - ${category}` : `${alcoholicOrNot}`}
         </span>
-        <div className="tags">
-          {tags.length > 0 &&
-            tags.map((tag) => (
-              <span
-                data-testid={`${index}-${tag}-horizontal-tag`}
-                key={`${name}${tag}`}
-                className="tag"
-              >
-                {tag}
-              </span>
-            ))}
-        </div>
+        <img data-testid={`${index}-horizontal-share-btn`} src={shareIcon} alt="Share Recipe" />
+      </div>
+      <Link
+        to={`/${type}s/${id}`}
+        data-testid={`${index}-horizontal-name`}
+        className="recipe-title"
+      >
+        {name}
+      </Link>
+      <span data-testid={`${index}-horizontal-done-date`} className="date-made">
+        Feita em: {doneDate}
+      </span>
+      <div className="tags">
+        {tags.length > 0 &&
+          tags.map((tag) => (
+            <span
+              data-testid={`${index}-${tag}-horizontal-tag`}
+              key={`${name}${tag}`}
+              className="tag"
+            >
+              {tag}
+            </span>
+          ))}
       </div>
     </div>
-  );
+  </div>
+);
+
+DoneRecipesCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  recipe: PropTypes.object.isRequired,
 };
 
 export default DoneRecipesCard;
--- src/components/RecipesButtons/index.js
+++ src/components/RecipesButtons/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 import './styles.css';
 
-const RecipesButtons = () => {
-  return (
-    <div className="recipes-buttons-container">
-      <button className="recipes-button" data-testid="filter-by-all-btn">All</button>
-      <button className="recipes-button" data-testid="filter-by-food-btn">Food</button>
-      <button className="recipes-button" data-testid="filter-by-drink-btn">Drinks</button>
-    </div>
-  );
-};
+const RecipesButtons = () => (
+  <div className="recipes-buttons-container">
+    <button className="recipes-button" data-testid="filter-by-all-btn">
+      All
+    </button>
+    <button className="recipes-button" data-testid="filter-by-food-btn">
+      Food
+    </button>
+    <button className="recipes-button" data-testid="filter-by-drink-btn">
+      Drinks
+    </button>
+  </div>
+);
 
 export default RecipesButtons;
