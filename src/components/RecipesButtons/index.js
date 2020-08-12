28c295a1f03cacccce98d1e9cffb9a90e23ffe77
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-11 22:26:54 -0300

    Filtrando com sucesso.

--- src/components/RecipesButtons/index.js
+++ src/components/RecipesButtons/index.js
@@ -2,15 +2,32 @@ import React from 'react';
 
 import './styles.css';
 
-const RecipesButtons = () => (
+const filterByType = (allRecipes, setRecipesFilter, type) => {
+  const filteredRecipes = allRecipes.filter((item) => item.type === type);
+  setRecipesFilter([...filteredRecipes]);
+};
+
+const RecipesButtons = ({ allRecipes, setRecipesFilter }) => (
   <div className="recipes-buttons-container">
-    <button className="recipes-button" data-testid="filter-by-all-btn">
+    <button
+      className="recipes-button"
+      data-testid="filter-by-all-btn"
+      onClick={() => setRecipesFilter([])}
+    >
       All
     </button>
-    <button className="recipes-button" data-testid="filter-by-food-btn">
+    <button
+      className="recipes-button"
+      data-testid="filter-by-food-btn"
+      onClick={() => filterByType(allRecipes, setRecipesFilter, 'comida')}
+    >
       Food
     </button>
-    <button className="recipes-button" data-testid="filter-by-drink-btn">
+    <button
+      className="recipes-button"
+      data-testid="filter-by-drink-btn"
+      onClick={() => filterByType(allRecipes, setRecipesFilter, 'bebida')}
+    >
       Drinks
     </button>
   </div>
--- src/pages/MadeRecipes.js
+++ src/pages/MadeRecipes.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import DoneRecipesCard from '../components/DoneRecipesCards';
 import RecipesButtons from '../components/RecipesButtons';
 
 const MadeRecipes = () => {
   const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  const [recipesFilter, setRecipesFilter] = useState([]);
   return (
     <div>
       <Header title="Receitas Feitas" visible={false} />
-      <RecipesButtons />
-      {doneRecipes.map((recipe, index) => (
-        <DoneRecipesCard key={recipe.id} recipe={recipe} index={index} />
-      ))}
+      <RecipesButtons allRecipes={doneRecipes} setRecipesFilter={setRecipesFilter} />
+      {recipesFilter.length !== 0 &&
+        recipesFilter.map((recipe, index) => (
+          <DoneRecipesCard key={recipe.id} recipe={recipe} index={index} />
+        ))}
+      {recipesFilter.length === 0 &&
+        doneRecipes.map((recipe, index) => (
+          <DoneRecipesCard key={recipe.id} recipe={recipe} index={index} />
+        ))}
     </div>
   );
 };
