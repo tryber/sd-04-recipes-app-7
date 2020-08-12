53514fc0c1b1a95c6eb513083f10e95affc3cc53
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-12 16:58:58 -0300

    Copiando para o clipboard e estilo para botão.

--- src/components/ShareButton/index.js
+++ src/components/ShareButton/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import copyToClipboard from 'clipboard-copy';
 import PropTypes from 'prop-types'
 import shareIcon from '../../images/shareIcon.svg';
 
+const togglePopUp = (setPopUpVisible) => {
+  setPopUpVisible(true);
+  setTimeout(() => setPopUpVisible(false), 1500);
+};
+
 const ShareButton = ({ testid, path }) => {
+  const [popUpVisible, setPopUpVisible] = useState(false);
   const wayTo = `${window.location.host}${path}`;
   return (
-    <div>
-      <button className="share-button" onClick={() => copyToClipboard(wayTo)}
+    <div className="share-button-container">
+      {popUpVisible && <span className="pop-up-clipboard">Link copiado!</span>}
+      <button className="share-button" onClick={() => {
+        copyToClipboard(wayTo);
+        togglePopUp(setPopUpVisible);
+      }}
       >
         <img src={shareIcon} alt="share-icon" data-testid={testid} />
       </button>
