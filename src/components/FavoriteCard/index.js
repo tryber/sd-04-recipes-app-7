0da613a4b5aa6e6d8c66ce313efc1aa6c74bf594
Thiago Tavares <thiagooo15@gmail.com>
2020-08-19 00:23:28 -0300

    ccryagain

---
+++ src/components/FavoriteCard/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import FavoriteButon from '../../images/blackHeartIcon.svg';
+
+import ShareButton from '../ShareButton';
+
+import '../DoneRecipesCard/styles.css';
+
+const FavoriteCard = ({
+  recipe: { id, type, area, category, alcoholicOrNot, name, image, doneDate, tags },
+  index,
+}) => (
+  <div className="done-recipes-container">
+    <Link to={`/${type}s/${id}`} className="link-img-recipe">
+      <img data-testid={`${index}-horizontal-image`} src={image} alt={name} />
+    </Link>
+    <div className="recipes-info">
+      <div className="share-container">
+        <span data-testid={`${index}-horizontal-top-text`}>
+          {type === 'comida' ? `${area} - ${category}` : `${alcoholicOrNot}`}
+        </span>
+        <ShareButton testid={`${index}-horizontal-share-btn`} path={`/${type}s/${id}`} />
+      </div>
+      <Link
+        to={`/${type}s/${id}`}
+        data-testid={`${index}-horizontal-name`}
+        className="recipe-title"
+      >
+        {name}
+      </Link>
+      <button
+        id={id}
+        data-testid={`${index}-horizontal-favorite-btn`}
+      >
+        <img src={FavoriteButon} alt="Favorite Icon" />
+      </button>
+    </div>
+  </div>
+);
+
+FavoriteCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  recipe: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default FavoriteCard;
--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,84 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import Header from '../components/Header';
-import ShareButon from '../images/shareIcon.svg';
-import FavoriteButon from '../images/blackHeartIcon.svg';
+import FavoriteCard from '../components/FavoriteCard';
 import RecipesButtons from '../components/RecipesButtons';
 
-const createCards = (params, setUpdateUnfavorite) =>
-  params.map(
-    ({ id, type, area, category, alcoholicOrNot, name, image }, index) => {
-      const path = type === 'comida' ? `/comidas/${id}` : `/bebidas/${id}`;
-      return (
-        <div key={id}>
-          <div>
-            <Link to={path}>
-              <img src={image} alt={name} data-testid={`${index}-horizontal-top-text`} />
-            </Link>
-          </div>
-          <div>
-            {type === 'comida' ? (
-              <p data-testid={`${index}-horizontal-top-text`}>
-                {area} - {category}
-              </p>
-            ) : (
-              <p ata-testid={`${index}-horizontal-top-text`}>
-                {alcoholicOrNot}
-              </p>
-            )}
-            <Link to={path}>
-              <p data-testid={`${index}-horizontal-share-btn`} path={path} />
-            </Link>
-            <button testid={`${index}-horizontal-share-btn`} path={path}>
-              <img src={ShareButon} alt="Share Icon" />
-            </button>
-            <button
-              id={id}
-              handleElement={setUpdateUnfavorite}
-              testid={`${index}-horizontal-favorite-btn`}
-            >
-              <img src={FavoriteButon} alt="Favorite Icon" />
-            </button>
-          </div>
-        </div>
-      );
-    },
-  );
-
-const mountFavoriteList = (filter, favorites, setUpdateUnfavorite) => {
-  let mountParams = [];
-  if (favorites.length === 0) return <p>Sem receitas favoritas...</p>;
-  if (filter === 'comida') {
-    mountParams = favorites.filter((favorite) => favorite.type === 'bebida');
-    return createCards(mountParams, setUpdateUnfavorite);
-  }
-  mountParams = favorites;
-  return createCards(mountParams, setUpdateUnfavorite);
-};
 
 function FavoriteRecipes() {
-  const [updateUnfavorite, setUpdateUnfavorite] = useState(false);
-  const [filterRecipes] = useState('all');
-  const [favorites, setFavorites] = useState([]);
+  const [filterRecipes, setFilterRecipes] = useState([]);
 
-  const getFavoritesAndSet = () => {
-    const favoriteList = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
-    console.log(favoriteList);
-    setFavorites([...favoriteList]);
-    setUpdateUnfavorite(false);
-  };
-
-  useEffect(() => {
-    getFavoritesAndSet();
-  }, [updateUnfavorite]);
+  const favoriteList = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
 
   return (
     <div>
       <Header title="Receitas Favoritas" visible={false} />
-      <RecipesButtons />
-      {favorites && favorites.length >= 1
-        ? mountFavoriteList(filterRecipes, favorites, setUpdateUnfavorite)
-        : null}
+      <RecipesButtons allRecipes={favoriteList} setRecipesFilter={setFilterRecipes}  />
+      {filterRecipes.length !==0 &&
+        filterRecipes.map((recipe, index) => (
+          <FavoriteCard key={recipe.id} recipe={recipe} index={index} />
+        ))}
+      {filterRecipes.length === 0 &&
+        favoriteList.length !== 0 &&
+        favoriteList.map((recipe, index) => (
+          <FavoriteCard key={recipe.id} recipe={recipe} index={index} />
+        ))}
     </div>
   );
 }
