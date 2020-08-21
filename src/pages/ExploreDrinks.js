2485639b59993d8a68dece70e2479bf043c15f55
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-21 04:25:07 -0300

    Estilo para explorar bebidas feito.

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -4,6 +4,8 @@ import { randomDrinkId } from '../services';
 import BottomBar from '../components/BottomBar/BottomBar';
 import Header from '../components/Header';
 
+import './CSS/ExplorersBy.css';
+
 const ExploreDrinks = () => {
   const history = useHistory();
   const [id, setId] = useState('');
@@ -20,12 +22,14 @@ const ExploreDrinks = () => {
       <Header title="Explorar Bebidas" visible={false} />
       <button
         data-testid="explore-by-ingredient"
+        className="explorer-by-button"
         onClick={() => history.push('/explorar/bebidas/ingredientes')}
       >
         Por Ingredientes
       </button>
       <button
         data-testid="explore-surprise"
+        className="explorer-by-button"
         onClick={() => surpriseMe(history)}
       >
         Me Surpreenda!
